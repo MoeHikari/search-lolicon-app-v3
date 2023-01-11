6a42c5a6cf83a89766e354d5c95bf35b1fea9885
魂归梓里 <ziazusa@foxmail.com>
2023-01-11 16:58:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.config = {
-    setLicense: "123456",
-    setProxy: "i.pixiv.re",
-    setPixivAPI: "pixiv.re",
-    setSize: "regular",
-    setShowTags: 5,
-    maxNumber: 20,
-    hideR18: false,
-    htmlIcon: ""
+    setLicense: "123456", //许可码
+    setProxy: "i.pixiv.re", //Pixiv图片代理
+    setPixivAPI: "pixiv.re", //Pixiv PID查图API
+    setSize: "regular", //在网页展示的图片规格（original，regular，small，thumb，mini）
+    setShowTags: 5, //在网页展示的图片Tag数目，设置为0为全部展示
+    maxNumber: 20, //设置允许查找图片的最大数目（小于等于20）
+    hideR18: false, //设置是否屏蔽R18作品
+    htmlIcon: "" //设置网页图标
 }
